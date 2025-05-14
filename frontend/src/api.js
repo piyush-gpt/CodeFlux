bfe38f0d833a2726e8e45722e8de65eea4d077da
piyushgupta <piyushgpt10102004@gmail.com>
2025-05-14 14:24:14 +0530

    testing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const instance = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
 console.log(import.meta.env.VITE_API_URL);
 console.log(import.meta.env.VITE_POD_DEPLOYMENT_URL);
-console.log("hello lol")
+console.log("hello")
 export default instance;
